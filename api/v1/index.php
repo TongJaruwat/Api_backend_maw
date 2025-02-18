cd8c67f60b5b22565d04d322b105f97ee16cbc1f
TongJaruwat <118808485+TongJaruwat@users.noreply.github.com>
2025-02-19 04:02:25 +0700

    df

--- api/v1/index.php
+++ api/v1/index.php
@@ -4,11 +4,13 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-header('Access-Control-Allow-Origin: *');
+/* header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Credentials: true");
 header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 header('Access-Control-Max-Age: 1000');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization'); */
+
+header('Access-Control-Allow-Origin', '*');
 require_once './DbHandler.php';
 require_once '../include/Config.php';
 require '../../vendor/autoload.php';
